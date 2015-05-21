247c2185b23bae04b8eeee4a7d38995b652675a9
vagrant <vagrant@web>
2015-05-21 07:22:42 +0000

    php unset explain

--- php/unset.php
+++ php/unset.php
@@ -5,6 +5,10 @@ $b = &$a;
 unset($b);
 $b = 'world';
 echo $a;
+//output hello
+
+//http://php.net/manual/en/function.unset.php
+//If a variable that is PASSED BY REFERENCE is unset() inside of a function, only the local variable is destroyed. The variable in the calling environment will retain the same value as before unset() was called.
 
 function foo(&$bar) {
 	unset($bar);
